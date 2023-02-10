3218b636a3c340e60353ba2cc9c3bd282b30e87f
AxelYR <axelyaelreynaga3@gmail.com>
2023-02-09 22:24:48 -0600

    Termino de la practica 6 y comienzo de la practica 7

---
+++ intweb7/js/tiposdedatosyvariables.js
@@ -0,0 +1,11 @@
+var nombre="Axel";
+var edad=34.653;
+var tengomascotas=true;
+var tengotrabajo=false;
+var tengosueldo;//basio
+
+let nombre="Axel";
+let edad=34;
+let tengomascotas=true;
+let tengotrabajo=false;
+let tengosueldo;//basio
