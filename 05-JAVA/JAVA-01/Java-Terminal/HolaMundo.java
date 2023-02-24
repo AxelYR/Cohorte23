403d240209f5e3f06966a3c6a196f42e4d8f0060
AxelYR <axelyaelreynaga3@gmail.com>
2023-02-23 20:50:41 -0600

    Inicio de practicas con Java primer hola mundo y primera clase

---
+++ 05-JAVA/JAVA-01/Java-Terminal/HolaMundo.java
@@ -0,0 +1,8 @@
+import Saludo;
+
+class HolaMundo{
+	public static void main (String [] ch23){
+		//System.out.println("Hola mundo desde JAVA");
+
+	}	
+}
---
+++ 05-JAVA/JAVA-01/Java-Terminal/Saludo.java
@@ -0,0 +1,8 @@
+/*Esto es una clase*/
+class Saludo{
+//Esto es un metodo
+	void saludar(){
+		System.out.println("Hola de nuez desde JAVA OOP");
+	}
+
+}
